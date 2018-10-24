4407aa2016846e2af70e5a2204ba4a393a41c2a7
Lukasz Czerniak <lfczerniak@gmail.com>
2018-10-24 08:04:31 +0200

    initial commit

---
+++ first_scrapy/first_scrapy/items.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+from scrapy.item import Item, Field
+
+
+class NewItem(scrapy.Item):
+    # define the fields for your item here like:
+    name = scrapy.Field()
+
+    main_headline = Field()
+    headline = Field()
+
+    url = Field()
+    project = Field()
+    spider = Field()
+    server = Field()
+    date = Field()
+
+    location = Field()
+    
+class TestItem(scrapy.Item):
+	id = scrapy.Field()
+	name = scrapy.Field()
+	description = scrapy.Field()
+
+class NewCamp(scrapy.Item):
+	name = Field()
+	url = Field()
+	region = Field()
+	capacity = Field()
--- first_scrapy/first_scrapy/spiders/camping_spider.py
+++ first_scrapy/first_scrapy/spiders/camping_spider.py
@@ -3,27 +3,38 @@ from first_scrapy.items import NewCamp
 
 class CampingSpider(scrapy.Spider):
 	name = "CampingSpider"
-
+	url = []
 	def start_requests(self):
 
 		allowed_domains = [
 			'www.camping.hr',
 		]
-		start_urls = ['https://www.camping.hr/croatian-campsites',
-		'https://www.camping.hr/croatian-campsites-p2',
 
-		]
+		start_urls = ['https://www.camping.hr/croatian-campsites']
+
 		for url in start_urls:
 			yield scrapy.Request(url=url, callback=self.parse)
-
 	
 	def parse(self, response):
-		for i in range(1,21):
+		for camp in response.xpath('//*[@id="the-serp"]/li'):
 			item = NewCamp()
-			item['name']=response.xpath('//*[@id="the-serp"]/li[{}]/div[2]/h2/a/strong/text()'.format(i)).extract()
-			item['region']=response.xpath('//*[@id="the-serp"]/li[{}]/div[2]/div[1]/text()'.format(i)).extract()[0].split('(')[1].split(')')[0]
-			item['url']=response.xpath('//*[@id="the-serp"]/li[{}]/div[2]/h2/a/@href'.format(i)).extract()
-			item['capacity']=response.xpath('//*[@id="the-serp"]/li[{}]/div[2]/div[2]/ul/li[2]/strong/text()'.format(i)).extract()
+			item['name']=camp.xpath('div[2]/h2/strong/text()').extract()
 
 			yield item
-		
+
+
+		# for i in range(1,21):
+		# 	item = NewCamp()
+		# 	item['name']=response.xpath('//*[@id="the-serp"]/li[{}]/div[2]/h2/a/strong/text()'.format(i)).extract()
+		# 	item['region']=response.xpath('//*[@id="the-serp"]/li[{}]/div[2]/div[1]/text()'.format(i)).extract()[0].split('(')[1].split(')')[0]
+		# 	item['url']=response.xpath('//*[@id="the-serp"]/li[{}]/div[2]/h2/a/@href'.format(i)).extract()
+		# 	item['capacity']=response.xpath('//*[@id="the-serp"]/li[{}]/div[2]/div[2]/ul/li[2]/strong/text()'.format(i)).extract()
+
+		# 	yield item
+
+		next_page = response.xpath('//*[@id="top-serp-nav"]/p[3]/a[6]/@href').extract_first()
+		if next_page is not None:
+			next_page = 'https://www.camping.hr' + str(next_page)
+			self.url.append(next_page)
+			print(self.url)
+			yield scrapy.Request(next_page, callback=self.parse)
